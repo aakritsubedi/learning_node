031e27c0a6d99f19f2df26d4c475563160a98dc3
aakritsubedi <aakritsubedi9@gmail.com>
2021-06-18 11:53:05 +0545

    Todo using Fastify

---
+++ todo-fastify/src/index.js
@@ -0,0 +1,23 @@
+const fastify = require("fastify")({
+  logger: true,
+});
+
+// Routes
+fastify.get("/", async (request, reply) => {
+  return {
+    app: "Learning Fastify",
+    version: "v1.0.0",
+    author: "Aakrit Subedi",
+  };
+});
+
+// Starting a server
+const PORT = 5000;
+(async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (err) {
+    fastify.log.error(`Unable to start server at ${address}.`);
+    process.exit(1);
+  }
+})();
